099d129559f01c842ec409bc078b4639d6355489
alanmath <alanmatheus187@gmail.com>
2024-07-14 19:17:45 -0300

    testing SAST vulnerability

--- src/main/java/insper/collie/gateway/GatewayConfiguration.java
+++ src/main/java/insper/collie/gateway/GatewayConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
 
 @Configuration
 public class GatewayConfiguration {
@@ -11,7 +15,28 @@ public class GatewayConfiguration {
     @Bean
     @LoadBalanced
     public WebClient.Builder loadBalancedWebClientBuilder() {
-        return WebClient.builder();
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    }
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    }
+                }
+            };
+            
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            
+            return WebClient.builder()
+                    .clientConnector(new ReactorClientHttpConnector(HttpClient.create()
+                        .secure(t -> t.sslContext(sslContext))));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
     
 }
